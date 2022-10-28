fcd2a20610e0501a7f784bdf582f2192ce76ae5d
Evgenia Sivtsova <105357551+EvSivtsova@users.noreply.github.com>
2022-10-28 20:15:50 +0100

    Add 1 test #factorial - hardcoded

--- calculator/spec/calculator_spec.rb
+++ calculator/spec/calculator_spec.rb
@@ -7,15 +7,25 @@ describe Calculator do
 
       expect(calculator.add(5, 10)).to eq(15)
     end
+
     it "returns a sum of 9 and 8" do
       calculator = Calculator.new
 
       expect(calculator.add(9, 8)).to eq(17)
     end
+
     it "returns a sum of 9 and 10" do
       calculator = Calculator.new
 
       expect(calculator.add(9, 10)).to eq(19)
+    end
+  end
+
+  describe "#factorial" do
+    it "returns 1 when given 0" do
+      calculator = Calculator.new
+
+      expect(calculator.factorial(0)).to eq(1)
     end
   end
 end
